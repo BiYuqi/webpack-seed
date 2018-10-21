82d63f3792f867a9d2187736d9f258e5e7d1c108
biyuqi <biyuqiwan@163.com>
2018-10-21 16:04:55 +0800

    :sparkles: 增加页面模板，支持ajax异步渲染模板

--- src/views/index/index.js
+++ src/views/index/index.js
@@ -4,5 +4,32 @@
 import '@/common/js/base'
 // 测试用的工具函数
 import { indexPage } from 'utils/tools'
+import EjsTest from '@/templates/index.test.ejs'
 
 document.querySelector('.index-title').innerHTML = indexPage()
+
+// 测试ejs模板使用
+const data = [
+  {
+    link: 'html/about.html',
+    key: '关于页面'
+  },
+  {
+    link: 'html/setting.html',
+    key: '设置页面'
+  },
+  {
+    link: 'html/news-tech.html',
+    key: '多目录测试'
+  }
+]
+// 注入模板数据
+// 经过测试 ajax异步返回数据, 打包后亦正常渲染
+$('.ejs-dynamic-inject').html(EjsTest({
+  index: data
+}))
+// setTimeout(() => {
+//   $('.ejs-dynamic-inject').html(EjsTest({
+//     index: data
+//   }))
+// }, 1000)
