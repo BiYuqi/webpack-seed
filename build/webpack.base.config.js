4fdb5e67bc369dea1fe89e2b39e15e34c7f3b002
biyuqi <biyuqiwan@163.com>
2018-10-24 23:53:57 +0800

    :sparkles: 新增处理iconfont配置

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -90,6 +90,14 @@ module.exports = {
           limit: 10000,
           name: 'media/[name].[hash:7].[ext]'
         }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'fonts/[name].[hash:7].[ext]'
+        }
       }
     ]
   },
