bcd0b9db2d26ae3c262db8fd298de13e0596e7b9
loadingmore <biyuqiwan@163.com>
2018-10-29 17:14:33 +0800

    :sparkles: add dom tools

---
+++ src/common/utils/dom.js
@@ -0,0 +1,24 @@
+class MinJQ {
+  constructor(selector) {
+    this.elem = []
+
+  }
+  hasClass() {
+    return this
+  }
+  addClass(cls) {
+    // this.
+    return this
+  }
+  removeClass() {
+    return this
+  }
+  toggleClass() {}
+  on() {}
+  html() {}
+  val() {}
+  prop() {}
+  attr() {}
+  getStyle() {}
+  css() {}
+}
