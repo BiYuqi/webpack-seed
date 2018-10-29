9d9f7a0f7ca9c0eeb92bd6d2766316bb45337887
loadingmore <biyuqiwan@163.com>
2018-10-29 13:37:00 +0800

    :sparkles: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,17 @@ module.exports = {
   env: {
     browser: true
   },
+  // rules: {
+  //   "规则名": [规则值, 规则配置]
+  // }
+  // "off"或者0    //关闭规则关闭
+  // "warn"或者1   //在打开的规则作为警告（不影响退出代码）
+  // "error"或者2   //把规则作为一个错误（退出代码触发时为1）
   rules: {
-    // 缩进2
-    "indent": ["error", 2],
+    "indent": ["error", 2], // 缩进2
     'generator-star-spacing': 'off',
-    "quotes": [ "error", "single"]
+    "quotes": ["error", "single"], // 默认使用单引号
+    "eqeqeq": 2, //必须使用全等
+    "default-case": 2 //switch语句最后必须有default
   }
 }
