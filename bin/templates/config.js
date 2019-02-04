802d9838f837aead3070dec61cf83f16bc85da40
biyuqi <biyuqiwan@163.com>
2019-02-05 00:12:14 +0800

    set templete config

---
+++ bin/templates/config.js
@@ -0,0 +1,12 @@
+// 模板文件夹名字 & 文件名
+// 供生成页面使用
+module.exports = {
+  standed: {
+    dir: 'layout',
+    name: 'layout'
+  },
+  notStanded: {
+    dir: 'layoutAuth',
+    name: 'layoutAuth'
+  }
+}
