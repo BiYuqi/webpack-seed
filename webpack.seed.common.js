3fd042999089a99ce77166869b87204401273206
biyuqi <biyuqiwan@163.com>
2019-03-20 23:19:09 +0800

    format code

--- webpack.seed.common.js
+++ webpack.seed.common.js
@@ -9,34 +9,34 @@ module.exports = {
    * 本项目由于需要在github的gitpage预览,所以配置了二级目录以便预览
    * @type {String}
    * @default '/'
-  */
+   */
   BUILD_PATH: process.env.env_config === 'git' ? '/webpack-seed/' : '/',
   /**
    * 部署应用生成的生产环境构建文件的目录, 打包输出文件夹名 默认dist不建议修改
    * @param {string}
    * @default 'dist'
-  */
+   */
   OUT_PUT_FOLDER_NAME: 'dist',
   /**
    * 本地开发 webpack-dev-server的选项配置
    * @param {string}
    * @default 'dist'
-  */
+   */
   NORMAL_PAGE_PATH: path.resolve(__dirname, './src/views'),
   /**
    * 页面逻辑名称(即src/views/各个模块内部名称) 默认index.js
-   * @type {String} 
+   * @type {String}
    * @default 'index'
-  */
+   */
   STATIC_JS_NAME: 'index',
   /**
    * 页面模板默认名称 默认tpl.ejs
    * @type {String}
    * @default 'tpl'
-  */
+   */
   STATIC_TEMPLATE_NAME: 'tpl',
   /**
    * 页面输出html文件夹命名
-  */
+   */
   OUT_PUT_HTML_FOLDER: 'html'
 }
