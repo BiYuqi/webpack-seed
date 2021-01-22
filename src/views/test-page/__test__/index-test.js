b3afc7a5e52c08f4831cd3b83d0ba498a6cbee94
BiYuqi <biyq@mti-sh.cn>
2021-01-22 23:20:33 +0800

    chore: remove test

--- src/views/test-page/__test__/index-test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai'
-
-describe('What is the good unit test?', () => {
-  it('should render correct value', () => {
-    const res = 'good test'
-    expect(res).to.equal('good test')
-  })
-})
