2ef2e8f26c0669a0c789714aeb22a594b9e1df87
BiYuqi <biyuqiwan@163.com>
2021-01-24 22:38:40 +0800

    chore: add babel plugin

--- babel.config.js
+++ babel.config.js
@@ -11,5 +11,9 @@ module.exports = {
       }
     ]
   ],
-  plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-optional-chaining']
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-optional-chaining'
+  ]
 }
