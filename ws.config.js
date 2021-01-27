9dacb2eb1a18941b95156bf94c600e0454ac80b5
GeekMakerSun <mail@sunbingchen.cn>
2021-01-27 08:15:43 +0800

    Update ws.config.js

--- ws.config.js
+++ ws.config.js
@@ -1,4 +1,14 @@
+const isProd = process.env.WS_ENV === 'production'
+
 module.exports = {
   baseDir: '/',
-  chainWebpack: config => {}
+  chainWebpack: config => {
+    /**
+     * 开发环境开启全量更新
+     * 目前只有js scss改动会热更新, ejs文件并不会热更新，如果需要更新, 请配置如下:
+     */
+    if (!isProd) {
+      config.devServer.watchContentBase(true)
+    }
+  }
 }
